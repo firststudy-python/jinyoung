d1ca35cbb4dc0387232ffe1e6a5fe892ef704b75
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-28 13:23:10 +0900

    1주차 과제 답입니다.

---
+++ 1주차/1주차 과제와 답.py
@@ -0,0 +1,17 @@
+#1주차 과제
+
+# 001 화면에 Hello World 문자열을 출력하세요.
+# 답: print("Hello World") / print('Hello World')
+
+# 002 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+# 답: print("Mary's cosmetics")
+
+#003 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
+#    신씨가 소리질렀다. "도둑이야".
+#답: print('신씨가 소리질렀다. "도둑이야"')
+
+#004 화면에 "C:\Windows"를 출력하세요.
+#답:print("C:\Windows")
+
+#005 5/3의 결과를 화면에 출력하세요.
+#답: print(5/3)
